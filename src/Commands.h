86e4e1f107bcd97388510c700b58b47b5f6324c6
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-12-10 19:57:01 +0100

    splitting CREATE_COMMAND_DECLARATION macro into command parser and command executer parts

--- src/Commands.h
+++ src/Commands.h
@@ -2,18 +2,23 @@
 
 #include "Command.h"
 
-
+#define CREATE_COMMAND_PARSER_DECLARATION(Name)\
+    class Name##CommandParser: public CommandParser{\
+    public:\
+        virtual CommandExecuterPtr parse(const CommandLine &line);\
+        static std::string command_name();\
+    };
+    
+#define CREATE_COMMAND_EXECUTER_DECLARATION(Name)\
+    class  Name##CommandExecuter: public CommandExecuter{\
+    public: \
+         virtual std::string execute(State &state);\
+         virtual bool exit_program();\
+    };
+    
 #define CREATE_COMMAND_DECLARATION(Name)\
-class  Name##CommandExecuter: public CommandExecuter{\
-public: \
-     virtual std::string execute(State &state);\
-     virtual bool exit_program();\
-};\
-class Name##CommandParser: public CommandParser{\
-public:\
-    virtual CommandExecuterPtr parse(const CommandLine &line);\
-    static std::string command_name();\
-};
+    CREATE_COMMAND_EXECUTER_DECLARATION(Name);\
+    CREATE_COMMAND_PARSER_DECLARATION(Name);
 
 CREATE_COMMAND_DECLARATION(Score);
 CREATE_COMMAND_DECLARATION(Exit);
