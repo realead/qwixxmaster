1ed7b4b9e8854a3d85f4334f5172c52e605808d1
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-12-10 19:37:47 +0100

    introducing define for declaring command parser/executer

--- src/Commands.h
+++ src/Commands.h
@@ -2,31 +2,20 @@
 
 #include "Command.h"
 
-//Score:
 
-class ScoreCommandExecuter: public CommandExecuter{
-public: 
-     virtual std::string execute(State &state);
-     virtual bool exit_program();
+#define CREATE_COMMAND_DECLARATION(Name)\
+class  Name##CommandExecuter: public CommandExecuter{\
+public: \
+     virtual std::string execute(State &state);\
+     virtual bool exit_program();\
+};\
+class Name##CommandParser: public CommandParser{\
+public:\
+    virtual CommandExecuterPtr parse(const CommandLine &line);\
+    static std::string command_name();\
 };
 
-class ScoreCommandParser: public CommandParser{
-public:
-    virtual CommandExecuterPtr parse(const CommandLine &line);
-    static std::string command_name();
-};
+CREATE_COMMAND_DECLARATION(Score);
+CREATE_COMMAND_DECLARATION(Exit);
 
-
-//Exit
-class ExitCommandExecuter: public CommandExecuter{
-public: 
-     virtual std::string execute(State &state);
-     virtual bool exit_program();
-};
-
-class ExitCommandParser: public CommandParser{
-public:
-    virtual CommandExecuterPtr parse(const CommandLine &line);
-    static std::string command_name();
-};
 
