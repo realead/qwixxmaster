079c1f841f0bf1fecd487cd4579a8c1659fbbddb
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-12-27 16:36:19 +0100

    extracting evaluate_without_whites function

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -75,6 +75,18 @@ float Evaluator::evaluate_state(const State &state){
 
 }
 
+float Evaluator::evaluate_without_whites(const State &state, const DiceRoll &roll){
+    // no whites!
+    float best=-1e6;
+    for(size_t dice=4;dice<=5;dice++)
+        for(size_t j=0;j<COLOR_CNT;j++){
+            Color color=static_cast<Color>(j);
+            State cur=state;
+            if(cur.take(color, roll[color]+roll[dice]))
+                   best=std::max(best, evaluate_state(cur));
+        } 
+    return best;
+}
 
 float Evaluator::evaluate_roll(const State &state, const DiceRoll &roll){
 
@@ -104,16 +116,9 @@ float Evaluator::evaluate_roll(const State &state, const DiceRoll &roll){
         }
     }
     
-    // no whites!
-    for(size_t dice=4;dice<=5;dice++)
-        for(size_t j=0;j<COLOR_CNT;j++){
-            Color color=static_cast<Color>(j);
-                 cur=state;
-                 if(cur.take(color, roll[color]+roll[dice]))
-                       best=std::max(best, evaluate_state(cur));
-        }  
+     
     
-    return best;
+    return std::max(best, evaluate_without_whites(state, roll));
 }
 
 
--- src/Evaluator.h
+++ src/Evaluator.h
@@ -9,7 +9,9 @@ class State;
 class Evaluator{
 private:
     size_t sampling_number;
-    std::vector<float> mem;   
+    std::vector<float> mem;
+    
+    float evaluate_without_whites(const State &state, const DiceRoll &roll);    
 public:
     Evaluator(size_t sampling_number);
     float evaluate_state(const State &state);
